975419876784fb80b1e723016084d912d109c8b6
Jorge-2005 <181957518+Jorge-2005@users.noreply.github.com>
2025-12-02 17:38:42 +0100

    Add files via upload

---
+++ M1/ft_printf/ft_printf.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jovillal <jovillal@student.42malaga.c>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/25 17:11:16 by jovillal          #+#    #+#             */
+/*   Updated: 2025/11/25 17:11:25 by jovillal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *type, ...);
+int	ft_putchar(char c);
+int	ft_putstr(char *s);
+int	ft_putpoint(void *str);
+int	ft_putnbr(int n);
+int	ft_putnbr_u(unsigned int n);
+int	ft_hexmin(unsigned int n);
+int	ft_hex(unsigned int n);
+#endif

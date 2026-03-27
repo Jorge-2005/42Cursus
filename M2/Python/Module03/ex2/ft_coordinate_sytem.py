d3242cbf680c2217acbb852e10cae57549dffbc6
Jorge-2005 <181957518+Jorge-2005@users.noreply.github.com>
2026-03-27 19:49:53 +0100

    Add files via upload

---
+++ M2/Python/Module03/ex2/ft_coordinate_sytem.py
@@ -0,0 +1,7 @@
+def get_player_pos() -> None:
+    coordinates: str = input("Enter new coordinates as "
+                             "floats in format 'x,y,z': ")
+    print(f"{coordinates}")
+
+
+get_player_pos()

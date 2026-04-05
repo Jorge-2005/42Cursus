13f701d595cf917b99c35a01758cf5641604017c
Jorge-2005 <181957518+Jorge-2005@users.noreply.github.com>
2026-04-05 09:48:16 +0200

    Delete M2/Python/Module03 directory

--- M2/Python/Module03/ex2/ft_coordinate_sytem.py
+++
@@ -1,7 +0,0 @@
-def get_player_pos() -> None:
-    coordinates: str = input("Enter new coordinates as "
-                             "floats in format 'x,y,z': ")
-    print(f"{coordinates}")
-
-
-get_player_pos()
